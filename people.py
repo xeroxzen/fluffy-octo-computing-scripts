a0caa0d3fa018251aaaa414cfcf1b9e2c1f975ea
[Google Jr] <andilembele020@gmail.com>
2020-10-10 12:01:55 +0200

    Created people.py. Still got an error till I find out the alternative for mem_profile

---
+++ people.py
@@ -0,0 +1,44 @@
+import random
+import time
+from past.builtins import xrange
+
+names = ['Andile', 'Eric', 'Nandipa', 'Phumz', 'Zandile', 'Proud', 'Clifton']
+majors = ['Engineering', 'Data Science', 'Biomedicine', 'Computer Science', 'Chemistry', 'Design']
+
+print('Memory (Before): {}Mb'.format(mem_profile.memory_usage_resource()))
+
+
+def people_list(num_people):
+    result = []
+    for i in xrange(num_people):
+        person = {
+            'id': i,
+            'name': random.choice(names),
+            'major': random.choice(majors)
+        }
+
+        result.append(person)
+    return result
+
+
+def people_generator(num_people):
+    for i in xrange(num_people):
+        person = {
+            'id': i,
+            'name': random.choice(names),
+            'major': random.choice(majors)
+        }
+
+        yield person
+
+
+t1 = time.clock()
+people = people_list(1000000)
+t2 = time.clock()
+
+t1 = time.clock()
+people = people_generator(1000000)
+t2 = time.clock()
+
+print('Memory (After) : {}Mb'.format(mem_profile.memory_usage_resource()))
+print('Took {} seconds'.format(t2-t1))
